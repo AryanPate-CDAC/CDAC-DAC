67a5b08966d7f0115c346c911624f3d74e390e6e
AryanPate-CDAC <aryan.pate.cmaug25@gmail.com>
2025-08-28 02:38:01 +0530

    OOPJ Assignment1 1-10 questions

---
+++ Greatest_Scores.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Greatest_Scores {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter score for Test 1: ");
+        int test1 = sc.nextInt();
+
+        System.out.print("Enter score for Test 2: ");
+        int test2 = sc.nextInt();
+
+        if (test1 > test2) {
+            System.out.println("Test 1 has higher score.");
+        } else if (test2 > test1) {
+            System.out.println("Test 2 has higher score.");
+        } else {
+            System.out.println("Both tests have the same score.");
+        }
+
+        sc.close();
+    }
+}
---
+++ ShopDiscount.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ShopDiscount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter total purchase amount: ");
+        double amount = sc.nextDouble();
+
+        if (amount > 1000) {
+            amount = amount - (amount * 0.10);
+        }
+
+        System.out.println("Final cost after discount: " + (int)amount);
+
+        sc.close();
+    }
+}
